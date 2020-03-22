f3d94c90e2769324a9eb4fa24662390a1ad9c410
akshat2205 <akshatpal1232@gmail.com>
2020-03-22 18:30:23 +0530

    Images and background added

--- flappyBird.py
+++ flappyBird.py
@@ -1,5 +1,4 @@
-#The flappybird game
-#!/usr/bin/env python
+#Flappy bird
 import pygame
 import random
 
@@ -19,6 +18,14 @@ class FlappyBird:
 	clock = pygame.time.Clock()
 	run = False
 	upForce = 5
+	game_out = 10
+	score = 0
+
+	#message to inform
+	message = [
+		"You Crashed", "Score", "Game Over",
+		"Play", "Play Again"
+	]
 
 	def flyBird(self):
 		self.fps = 50
@@ -30,6 +37,13 @@ class FlappyBird:
 		self.pipeA_length = random.randint(60, 200)
 		self.pipeB_length = -random.randint(60, 200)
 		self.pipe_width = 30
+		self.lives = 5
+		self.touch = False
+		self.bird_size = 15
+		self.backgroundImage = pygame.image.load("cityBackground.jpg").convert_alpha()
+		self.backgroundImage = pygame.transform.scale(self.backgroundImage, (self.width, self.height))
+		self.birdImage = pygame.image.load("bird_wing_down.png").convert_alpha()
+		self.groundImage = pygame.image.load("ground.png").convert_alpha()
 
 		while not self.run:
 			for event in pygame.event.get():
@@ -41,46 +55,67 @@ class FlappyBird:
 					if event.key == pygame.K_SPACE:
 						self.velocity += -self.gravity*15
 
+
 			self.window.fill((130,255,255))
 			#adding gravity
 			self.velocity += self.gravity
 			self.y_pos += self.velocity
 
-			#changing pipe x-coordinate
+			#changing pipe x-coordinate  
 			self.pipe_ax -= 2
 			self.pipe_bx -= 2
 
-			print("Pipe a x-coordinate: ", self.pipe_ax)
-			print("Pipe a y-coordinate: ", self.pipe_ay)
-			print("Pipe b x-coordinate: ", self.pipe_bx)
-			print("Pipe b y-coordinate: ", self.pipe_by)
-			#Adding pipes
+			# print("Pipe ax-coordinate: ", self.pipe_ax)
+			# print("Pipe ay-coordinate: ", self.pipe_ay)
+			# print("Pipe bx-coordinate: ", self.pipe_bx)
+			# print("pipe by-coordinate: ", self.pipe_by)
+			# Adding pipes
 			self.window.fill((255,255,255))
-			pygame.draw.rect(self.window, (0,0,0), [self.pipe_ax, self.pipe_ay, self.pipe_width, self.pipeA_length])
-			pygame.draw.rect(self.window, (0,0,0), [self.pipe_bx, self.pipe_by, self.pipe_width, self.pipeB_length])
+			self.window.blit(self.backgroundImage, [0, 0])
+			self.window.blit(self.groundImage, [0, 350])
+			pygame.draw.rect(self.window, (0,0,180), [self.pipe_ax, self.pipe_ay, self.pipe_width, self.pipeA_length])
+			pygame.draw.rect(self.window, (0,0,180), [self.pipe_bx, self.pipe_by, self.pipe_width, self.pipeB_length])
 			pygame.display.update()
 
-			#pipe spawn point
+			#pipe spawn poin
 			if self.pipe_ax <= 0:
 				self.pipe_ax = self.width
-				self.pipeA_length = random.randint(70,200)
+				self.pipeA_length = random.randint(70,150)
+
 			if self.pipe_bx <= 0:
 				self.pipe_bx = self.width
-				self.pipeB_length = -random.randint(70,200)
-
+				self.pipeB_length = -random.randint(70,150)
+                                                                                    
 			#keeping the bird on screen
 			if self.y_pos > self.height:
 				self.y_pos = self.height
 				self.velocity = 0
-			if self.y_pos <= 0:
-				self.y_pos = self.height
+			if self.y_pos < 0:
+				self.y_pos = 0
 				self.velocity = 0
 			#Bird
-			pygame.draw.circle(self.window, (0,0,0), [self.x_pos, self.y_pos], 15)
+			self.window.blit(self.birdImage, [self.x_pos, self.y_pos])
 			pygame.display.update()
 			self.clock.tick(self.fps)
 
-#Main Class Method call
+			#Game crash condition
+			#True if player hits the boundry
+			if self.y_pos >= self.height or self.y_pos <= 0:
+				self.run = True
+				print("You crashed on boundry")
+			#True if player hits the pipe objects
+			#for pipe A (the Upper one)
+			if self.pipe_ax == self.x_pos:
+				self.score += 1
+				print("Your score: ", self.score)
+				if self.y_pos <= self.pipeA_length:
+					self.run = True
+					print("You crashed on pipe")
+					print("Pipe ax coordinate:", self.pipe_ax)
+					print("Pipe A Length: ", self.pipeA_length)
+					print("Bird y-coordinate: ", self.y_pos)
+			#Unable to add game exit condition for Pipe B (the lower one)
+
 start_game = FlappyBird()
 start_game.flyBird()
 pygame.quit()
