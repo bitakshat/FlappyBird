eb483c9b1eeb9cba07317d4266e92c883de9ae23
bitakshat <akshatpal1232@gmail.com>
2021-03-30 20:04:43 +0530

    [+] Bug fixed, add collision detection

--- flappyBird.py
+++ flappyBird.py
@@ -1,4 +1,4 @@
-#Flappy bird
+# Flappy bird
 import pygame
 import random
 
@@ -21,7 +21,7 @@ class FlappyBird:
 	game_out = 10
 	score = 0
 
-	#message to inform
+	# message to inform
 	message = [
 		"You Crashed", "Score", "Game Over",
 		"Play", "Play Again"
@@ -29,93 +29,95 @@ class FlappyBird:
 
 	def flyBird(self):
 		self.fps = 50
-		#ariables for pipe(initial)
-		self.pipe_ax = random.randint(0, self.width)
-		self.pipe_ay = 0
-		self.pipe_bx = self.pipe_ax
-		self.pipe_by = self.height
-		self.pipeA_length = random.randint(60, 200)
-		self.pipeB_length = -random.randint(60, 200)
-		self.pipe_width = 30
+		# initial variables for pipe
 		self.lives = 5
 		self.touch = False
 		self.bird_size = 15
-		self.backgroundImage = pygame.image.load("cityBackground.jpg").convert_alpha()
+		self.backgroundImage = pygame.image.load("./images/cityBackground.jpg").convert_alpha()
 		self.backgroundImage = pygame.transform.scale(self.backgroundImage, (self.width, self.height))
-		self.birdImage = pygame.image.load("bird_wing_down.png").convert_alpha()
-		self.groundImage = pygame.image.load("ground.png").convert_alpha()
+		self.birdImage = pygame.image.load("./images/bird_wing_down.png").convert_alpha()
+		self.groundImage = pygame.image.load("./images/ground.png").convert_alpha()
+		self.pipe_width = 30		# pipe width
+		self.pipe_x = self.width	# pipe x coordinate
+		self.pipe_y = 0				# pipe y coordinate
+		self.invisible_pipe_x = self.width
+		self.invisible_pipe_y = random.randint(50, 250)		# invisible pipe random y coordinate
+		self.invisible_pipe_len = random.randint(100, 250)	# random pipe length
 
 		while not self.run:
 			for event in pygame.event.get():
-				print(event)
+				# print(event)
 				if event.type == pygame.QUIT:
 					self.run = True
-				#adding movements
+				# adding movements
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_SPACE:
-						self.velocity += -self.gravity*15
-
+						self.velocity -= self.gravity*15
 
 			self.window.fill((130,255,255))
-			#adding gravity
+			# adding gravity for bird
 			self.velocity += self.gravity
 			self.y_pos += self.velocity
 
-			#changing pipe x-coordinate  
-			self.pipe_ax -= 2
-			self.pipe_bx -= 2
+			# changing pipe x-coordinate on render
+			self.pipe_x -= 2
+			self.invisible_pipe_x -= 2
 
-			# print("Pipe ax-coordinate: ", self.pipe_ax)
-			# print("Pipe ay-coordinate: ", self.pipe_ay)
-			# print("Pipe bx-coordinate: ", self.pipe_bx)
-			# print("pipe by-coordinate: ", self.pipe_by)
 			# Adding pipes
 			self.window.fill((255,255,255))
 			self.window.blit(self.backgroundImage, [0, 0])
 			self.window.blit(self.groundImage, [0, 350])
-			pygame.draw.rect(self.window, (0,0,180), [self.pipe_ax, self.pipe_ay, self.pipe_width, self.pipeA_length])
-			pygame.draw.rect(self.window, (0,0,180), [self.pipe_bx, self.pipe_by, self.pipe_width, self.pipeB_length])
+			pygame.draw.rect(self.window, (0,0,180), [self.pipe_x, self.pipe_y, self.pipe_width, self.height])
+			# for invisible pipe
+			pygame.draw.rect(self.window, (255, 255, 255), [self.invisible_pipe_x, self.invisible_pipe_y, 30, self.invisible_pipe_len])
 			pygame.display.update()
 
-			#pipe spawn poin
-			if self.pipe_ax <= 0:
-				self.pipe_ax = self.width
-				self.pipeA_length = random.randint(70,150)
+			# pipe spawn point
+			if self.pipe_x <= 0:
+				self.pipe_x = self.width
+			if self.invisible_pipe_x <= 0:
+				self.invisible_pipe_x = self.width
+				self.invisible_pipe_len = random.randint(100, 250)
+				self.invisible_pipe_y = random.randint(50, 170)
 
-			if self.pipe_bx <= 0:
-				self.pipe_bx = self.width
-				self.pipeB_length = -random.randint(70,150)
-                                                                                    
-			#keeping the bird on screen
+			# keeping the bird on screen
 			if self.y_pos > self.height:
 				self.y_pos = self.height
 				self.velocity = 0
 			if self.y_pos < 0:
 				self.y_pos = 0
 				self.velocity = 0
-			#Bird
+			# Bird
 			self.window.blit(self.birdImage, [self.x_pos, self.y_pos])
 			pygame.display.update()
 			self.clock.tick(self.fps)
 
-			#Game crash condition
-			#True if player hits the boundry
+			# Game crash condition
+			# True if player hits the boundry
 			if self.y_pos >= self.height or self.y_pos <= 0:
 				self.run = True
-				print("You crashed on boundry")
-			#True if player hits the pipe objects
-			#for pipe A (the Upper one)
-			if self.pipe_ax == self.x_pos:
+				print("You crashed: boundry")
+			# True if player hits the pipe objects
+			elif((self.x_pos == self.invisible_pipe_x) and (self.y_pos >= self.invisible_pipe_y+self.invisible_pipe_len or self.y_pos <= self.invisible_pipe_y)):
+				self.run = True
+				print("You crashed: Pipe")
+				print("bird y coordinate: ", self.y_pos)
+				print("pipe y coordinate: ", self.y_pos)
+			else:
 				self.score += 1
-				print("Your score: ", self.score)
-				if self.y_pos <= self.pipeA_length:
-					self.run = True
-					print("You crashed on pipe")
-					print("Pipe ax coordinate:", self.pipe_ax)
-					print("Pipe A Length: ", self.pipeA_length)
-					print("Bird y-coordinate: ", self.y_pos)
-			#Unable to add game exit condition for Pipe B (the lower one)
+			# don't forget to add score
 
-start_game = FlappyBird()
-start_game.flyBird()
+			# for pipe A (the Upper one)
+			# if self.pipe_x == self.x_pos:
+			# 	self.score += 1
+			# 	print("Your score: ", self.score)
+			# 	if self.y_pos <= self.pipeA_length:
+			# 		self.run = True
+			# 		print("You crashed: pipe")
+			# 		print("Pipe ax coordinate:", self.pipe_x)
+			# 		print("Pipe A Length: ", self.pipeA_length)
+			# 		print("Bird y-coordinate: ", self.y_pos)
+
+start = FlappyBird()
+start.flyBird()
 pygame.quit()
